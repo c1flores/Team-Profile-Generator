ef6839030bbd03b3faf8b6aa842da4a549d48e6a
Christian Flores <c1flores@ucsd.edu>
2022-10-12 16:37:15 -0700

    Add unit tests for Employee.js

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,34 @@
+const Employee = require ('..lib/Employee');
+
+//test to see if employee object is created
+test('creates an employee object', () => {
+    const employee = new Employee('Gustavo');
+});
+
+//test to see if employee object uses constructor to configure id
+test('set an id with constructor', () => {
+    const testValue = 100;
+    const emp = new Employee('Foo', testValue);
+    expect(emp.id).toBe(testValue);
+});
+
+//test to see if employee object uses constructor to configure email
+test('set email with constructor', () => {
+    const testValue = 'emp@gmail.com';
+    const emp = new Employee('Foo', 1, testValue);
+    expect(emp.email).toBe(testValue);
+});
+
+//test to see if employee object uses constructor to implement getRole() method properly
+test('verify if getRole() returns Employee', () => {
+    const testValue = 'Employee';
+    const emp = new Employee('Gustavo', 1, 'emp@gmail.com');
+    expect(emp.getRole()).toBe(testValue);
+});
+
+
+
+
+
+
+
